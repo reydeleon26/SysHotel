7ec865a86e00a6b133453ea04b8525cbd7117dbf
reydeleon26 <51687094+reydeleon26@users.noreply.github.com>
2019-06-10 22:31:23 -0400

    Update Rooms.cs

--- SysHotelv1/Models/Rooms.cs
+++ SysHotelv1/Models/Rooms.cs
@@ -8,10 +8,19 @@ namespace SysHotelv1.Models
     public partial class Rooms
     {
         public int Id { get; set; }
+        
+        [Required]
+        [Display(Name ="Tipo de Cama")]
         public RoomType RoomType { get; set; }
         public int RoomTypeId { get; set; }
+        
+        [Required]
+        [Display(Name ="Tipo de Cama")]
         public BedType BedType { get; set; }
         public int BedTypeId { get; set; }
+        
+        [Required]
+        [Display(Name ="Numero de Habitacion")]
         public string RoomNumber { get; set; }
         public ICollection<ReservationDetails> ReservationDetails { get; set; }
 
