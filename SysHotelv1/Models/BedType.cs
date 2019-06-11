9433cae028dea5dfd4af09f959b0f9905963d4bd
reydeleon26 <51687094+reydeleon26@users.noreply.github.com>
2019-06-10 22:47:32 -0400

    Update BedType.cs

--- SysHotelv1/Models/BedType.cs
+++ SysHotelv1/Models/BedType.cs
@@ -8,8 +8,15 @@ namespace SysHotelv1.Models
     public class BedType
     {
        public int Id { get; set; }
+        
+       [Required]
+	   [Display(Name ="Tipo de Habitacion")]
        public string Description { get; set; }
+        
+       [Display(Name = "Precio por Cama")]
+       [DataType(DataType.Currency)]
        public decimal PricerPerBed { get; set; }
+        
        public ICollection<Rooms> Rooms { get; set; }
        public ICollection<ReservationDetails> ReservationDetails { get; set; }
 
