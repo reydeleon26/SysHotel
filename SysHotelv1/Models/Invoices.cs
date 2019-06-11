70aca6d20f6a927b3520359ba7586cc2f21f9b70
reydeleon26 <51687094+reydeleon26@users.noreply.github.com>
2019-06-10 22:38:52 -0400

    Update Invoices.cs

--- SysHotelv1/Models/Invoices.cs
+++ SysHotelv1/Models/Invoices.cs
@@ -9,10 +9,25 @@ namespace SysHotelv1.Models
     {
         public int Id { get; set; }
         public int ReservationDetailsId { get; set; }
+        
+        [Required]
+        [Display(Name ="Detalle de la Reservacion")]
         public ReservationDetails ReservationDetails { get; set; }
+        
+        [Required]
+        [Display(Name ="Precio por Adulto")]
         public decimal PriceAdult { get; set; }
+        
+        [Required]
+        [Display(Name ="Precio por Niño")]
         public decimal PriceChild { get; set; }
+        
+        [Required]
+        [Display(Name ="Cantidad de Dias")]
         public int DayCount { get; set; }
+        
+        [Required]
+        [Display(Name ="Descuento de Temporada")]
         public decimal SeasonalDiscount { get; set; }
         public int Total { get; set; }
 
