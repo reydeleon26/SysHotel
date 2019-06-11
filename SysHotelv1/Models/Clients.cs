9e8f04d4fa57214129e63b5c5c3ec970f88d5d49
reydeleon26 <51687094+reydeleon26@users.noreply.github.com>
2019-06-10 22:44:25 -0400

    Update Clients.cs

--- SysHotelv1/Models/Clients.cs
+++ SysHotelv1/Models/Clients.cs
@@ -8,13 +8,34 @@ namespace SysHotelv1.Models
     public class Clients
     {
         public int Id { get; set; }
+        
+        [Required]
+	    [Display(Name ="Nombres")]
         public string FullName { get; set; }
-        public string Identification { get; set; }
+                
+        [Required]
+	    [Display(Name ="Apellidos")]
         public string LastName { get; set; }
+        
+        [Required]
+	    [Display(Name ="Documento de Identificacion")]
+        public string Identification { get; set; }
+        
+        [Display(Name = "Correo Electronico")]
+        [MinLength(10)]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        
         public int IdCountry { get; set; }
+        
+        [Required]
+	    [Display(Name ="Pais")]
         public Country Country { get; set; }
+        
+        [Display(Name = "Telefono")]
+        [DataType(DataType.PhoneNumber)]
         public string Phone { get; set; }
+       
         public ICollection<Reservation> Reservation { get; set; }
 
         
