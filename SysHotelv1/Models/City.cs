f1c54c6e9113b9732cab5cf55ae6e12ffbfa1e64
reydeleon26 <51687094+reydeleon26@users.noreply.github.com>
2019-06-10 22:45:51 -0400

    Update City.cs

--- SysHotelv1/Models/City.cs
+++ SysHotelv1/Models/City.cs
@@ -8,8 +8,14 @@ namespace SysHotelv1.Models
     public class City
     {
         public int Id { get; set; }
+        
+        [Required]
+	    [Display(Name ="Ciudad")]
         public string Name { get; set; }
         public int CountryId { get; set; }
+        
+        [Required]
+	    [Display(Name ="Pais")]
         public Country Country { get; set; }
 
     }
