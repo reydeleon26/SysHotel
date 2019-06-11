468f82e574a608d18ae43f81de3231be328f209b
reydeleon26 <51687094+reydeleon26@users.noreply.github.com>
2019-06-10 22:46:16 -0400

    Update BookingStatus.cs

--- SysHotelv1/Models/BookingStatus.cs
+++ SysHotelv1/Models/BookingStatus.cs
@@ -8,6 +8,9 @@ namespace SysHotelv1.Models
     public class BookingStatus
     {
         public int Id { get; set; }
+        
+        [Required]
+	    [Display(Name ="Estatus")]
         public string DescriptionStatus { get; set; }
         public ICollection<Reservation> Reservations { get; set; }
     }
