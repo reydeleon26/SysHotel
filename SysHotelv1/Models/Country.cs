d2b0b1e1311558b5fadb28d658753e7e50aead0a
reydeleon26 <51687094+reydeleon26@users.noreply.github.com>
2019-06-10 22:39:27 -0400

    Update Country.cs

--- SysHotelv1/Models/Country.cs
+++ SysHotelv1/Models/Country.cs
@@ -8,8 +8,15 @@ namespace SysHotelv1.Models
     public class Country
     {
         public int Id { get; set; }
+        
+        [Required]
+        [Display(Name ="Pais")]
         public string Name { get; set; }
+        
+        [Required]
+        [Display(Name ="Nacionalidad")]
         public string Nationaly { get; set; }
+        
         public ICollection<City> City { get; set; }
         public ICollection<Clients> Clients { get; set; }
 
